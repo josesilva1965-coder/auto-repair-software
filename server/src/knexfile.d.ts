102e8416d46406331b38529a66284432a7b56dcf
josesilva1965-coder <hoangkimngan724@gmail.com>
2025-08-08 23:15:36 +0100

    Add files via upload

---
+++ server/src/knexfile.d.ts
@@ -0,0 +1,6 @@
+// This file provides type definitions for the knexfile.js,
+// allowing it to be imported into TypeScript source files.
+declare module '../knexfile.js' {
+  const config: { [key: string]: import('knex').Knex.Config };
+  export default config;
+}
